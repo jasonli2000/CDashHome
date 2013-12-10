83e11488faaba0a2f6a14cafb1fca217e02e002d
Jason Li <jason.li@kitware.com>
2013-12-10 15:40:45 -0500

    Add clientinfo file for crinoth

---
+++ crinoth-cdash-clientinfo.cmake
@@ -0,0 +1,24 @@
+#---------------------------------------------------------------------------
+# Copyright 2011-2013 The Open Source Electronic Health Record Agent
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#---------------------------------------------------------------------------
+
+   # Cache only:
+   set(VISTA_CACHE_NAMESPACE "VISTA")
+   set(VISTA_CACHE_INSTANCE "VISTA")
+   set(TEST_VISTA_FRESH_CACHE_DAT_VISTA "C:/InterSystems/Cache/mgr/vista/CACHE.DAT")
+   set(TEST_VISTA_FRESH_CACHE_DAT_EMPTY "C:/InterSystems/Cache/mgr/vista/empty/CACHE.DAT")
+   # (Copy an empty cache.dat to create empty.dat)
+
+
